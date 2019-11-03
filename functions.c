091e372f284487b73dc4fc82af02bc770004303c
Derek Lao <dlao00@stuy.edu>
2019-11-02 21:12:00 -0400

    wrote random element function

--- functions.c
+++ functions.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 #include "song_node.h"
 
 // void print_list_debug(struct song_node * subject)
@@ -8,6 +9,17 @@
 //   printf("the value is %d and the pointer is %ld and the next pointer is %ld", (*subject).i, subject, (*subject).next);
 //   printf("\n");
 // }
+
+int size(struct song_node *subject)
+{
+  int answer = 0;
+  while(subject)
+  {
+    subject = subject->next;
+    answer ++;
+  }
+  return answer;
+}
 
 void print_list(struct song_node * subject)
 {
@@ -160,34 +172,14 @@ struct song_node * free_list(struct song_node * subject)
   return subject;
 }
 
-// struct song_node * remove_song_node(struct song_node *front, char * songName, char * songArtist);
-// {
-//   struct song_node *prev;
-//   // printf("front is %ld\n", front);
-//   while((*front).i == data)
-//   {
-//     struct song_node *temp = front;
-//     front = (*front).next;
-//     free(temp);
-//     temp = NULL;
-//   }
-//   // printf("front is %ld\n", front);
-//   struct song_node *frontMarker = front;
-//   while(front)
-//   {
-//     // printf("front before is %ld\n", front);
-//     prev = front;
-//     front = (*front).next;
-//     // printf("got up to here\n");
-//     while(front && (*front).i == data)
-//     {
-//       struct song_node *temp = front;
-//       front = (*front).next;
-//       (*prev).next = front;
-//       free(temp);
-//       temp = NULL;
-//     }
-//     // printf("front after is %ld\n", front);
-//   }
-//   return frontMarker;
-// }
+struct song_node * random_element(struct song_node *subject)
+{
+  srand(time(NULL));
+  int numberOfIterations = rand() % size(subject);
+  while(numberOfIterations)
+  {
+    subject = subject->next;
+    numberOfIterations --;
+  }
+  return subject;
+}
--- song_node.h
+++ song_node.h
@@ -10,6 +10,8 @@ struct song_node * table[27];
 
 void print_list_debug(struct song_node * subject);
 void print_list(struct song_node * subject);
+int sizeOfList(struct song_node *subject);
+
 struct song_node * insert(struct song_node * subject, char * songName, char * songArtist);
 // insert nodes in order alphabetical by Artist then by Song, returns pointer to inserted node
 
@@ -24,3 +26,6 @@ struct song_node * find_song(struct song_node * subject, char * songName, char *
 struct song_node * free_list(struct song_node * subject);
 struct song_node * remove_song_node(struct song_node *front, char * songName, char * songArtist);
 // find and return a pointer to the node before the freed node;
+
+struct song_node * random_element(struct song_node *subject);
+// Return a pointer to random element in the list.
